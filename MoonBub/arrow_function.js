220bcc169a10a91ca4f8bba22525b41951ef971d
mirimSunwoo <w2010@e-mirim.hs.kr>
2022-03-21 13:43:37 +0900

    arrow function

---
+++ MoonBub/arrow_function.js
@@ -0,0 +1,10 @@
+printChar = function(문자){
+    console.log('익명함수 '+문자);
+}
+printChar('a');
+
+printArrow = (문자) => {
+    console.log('arrow_function '+문자);
+}
+printArrow('k');
+
---
+++ MoonBub/function.js
@@ -0,0 +1,15 @@
+function 문자열출력함수(문자){
+    console.log(문자)
+}
+문자열출력함수('PC지킴이');
+
+
+// printChar = 문자출력함수;
+// printChar(문자'b');
+
+printChar = function (문자){
+    console.log(문자)
+}
+printChar('청소를 열심히 합시다')
+
+
