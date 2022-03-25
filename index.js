76169afcb35fd140f09e6e75d2f03cf0f6293803
mirimSunwoo <w2010@e-mirim.hs.kr>
2022-03-25 14:46:32 +0900

    web static server page

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const http = require('http');
+const fs = require("fs");
 const app = http.createServer(function (req,res){
+    let url = req.url;
+
+    if(url === '/')
+        url = '/index.html';
+    if(url === '/favicon.ico')
+        return res.writeHead(404);
     res.writeHead(200);
-    console.log(__dirname + req.url);
-    res.end("Hello World");
-})
-app.listen(80);
+    res.end(fs.readFileSync(__dirname + url));
+});
+app.listen(3333);
