2b7cc6f67b0b4f628440dde63258f273dc2ae3f9
mirimSunwoo <w2010@e-mirim.hs.kr>
2022-04-11 14:34:05 +0900

    web2-33 글 생성 - 파일생성과 리다이렉션

--- index.js
+++ index.js
@@ -79,9 +79,11 @@ const app = http.createServer(function (request, response) {
         const post = qs.parse(body);
         const title = post.title;
         const description = post.description;
+        fs.writeFile(`data/${title}`, description,"utf-8", function (err){
+          response.writeHead(302,{Location:`/?id=${title}`});
+          response.end();
+        })
       });
-      response.writeHead(200);
-      response.end('success');
     } else {
       response.writeHead(404)
       response.end('Not found')
