fccc2c740c07a140508685f297bdb63509c202d5
mirimSunwoo <w2010@e-mirim.hs.kr>
2022-04-11 13:59:48 +0900

    web2-32 글 생성 데이터 받기

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const http = require('http')
-const fs = require('fs')
-const url = require('url')
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const qs = require('querystring');
 
 function templateList(filelist){
   let list = '<ul>';
@@ -68,9 +69,22 @@ const app = http.createServer(function (request, response) {
          response.writeHead(200);
          response.end(template)
       });
+    }
+    else if(pathname === '/create_process'){
+      let body = '';
+      request.on('data',function (data){
+        body += body + data;
+      });
+      request.on('end',function (){
+        const post = qs.parse(body);
+        const title = post.title;
+        const description = post.description;
+      });
+      response.writeHead(200);
+      response.end('success');
     } else {
-    response.writeHead(404)
-    response.end('Not found')
-  }
+      response.writeHead(404)
+      response.end('Not found')
+    }
 })
 app.listen(3334)
