22784b04b21ca5d1021e5354bec11e05fc6c2502
mirimSunwoo <w2010@e-mirim.hs.kr>
2022-05-30 15:19:31 +0900

    web3-3 9.1 미들웨어 만들기(readdir처리)

--- index.js
+++ index.js
@@ -2,28 +2,34 @@ const express = require('express');
 const app = express();
 const template = require('./lib/template.js');
 const fs = require('fs');
-const compression = require("compression");
-
-
+const compression = require('compression');
 // body-parser (기존에는 const bodyParser = require('body-parser'); 필요)
 app.use(express.urlencoded({extended: false}));
-app.use(compression()); // 압축
+app.use(compression());   // 압축
+app.get('*', function (req, res, next) {
+  fs.readdir('./data/', function (err, data) {
+    req.filelist = data;
+    next();
+  });
+});
 
 app.get('/', function (req, res) {
-    fs.readdir('./data', function(err, filelist){
+    // fs.readdir('./data', function(err, filelist){
         const title = 'Welcome';
         // 웹 페이지의 본문 내용
         const description = 'Hello, Node.js';
         // 게시글의 목록
-        const list = template.List(filelist);
+        const list = template.List(req.filelist);
         const html = template.HTML(title, list, description
             , `<a href="/create">create</a>`);
         res.send(html);
-    });
+    // });
 })
+
+
 app.get('/page/:pageId', function (req, res) {
-    fs.readdir('./data', function(err, filelist){
-        const list = template.List(filelist);
+    // fs.readdir('./data', function(err, filelist){
+        const list = template.List(req.filelist);
         const id = req.params.pageId;
         fs.readFile(`./data/${id}`, 'utf8', function (error, description){
             const title = id;
@@ -36,12 +42,14 @@ app.get('/page/:pageId', function (req, res) {
                 </form>`);
             res.send(html);
         })
-    })
+    // })
 })
+
+
 app.get('/create', function (req, res){
-  fs.readdir('./data', function (err, filelist) {
+  // fs.readdir('./data', function (err, filelist) {
     const title = 'WEB - create';
-    const list = template.List(filelist);
+    const list = template.List(req.filelist);
     const html = template.HTML(title, list, `
           <form action="/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
@@ -54,7 +62,7 @@ app.get('/create', function (req, res){
           </form>
         `, '');
     res.send(html);
-  });
+  // });
 });
 
 app.post('/create_process', function(req, res){
@@ -67,11 +75,11 @@ app.post('/create_process', function(req, res){
 });
 
 app.get('/update/:pageId', function(req, res){
-  fs.readdir('./data', function (err, filelist) {
+  // fs.readdir('./data', function (err, filelist) {
     const id = req.params.pageId;
     fs.readFile(`data/${id}`, 'utf8', function (error, description) {
       const title = id;
-      const list = template.List(filelist);
+      const list = template.List(req.filelist);
       const html = template.HTML(title, list,
         `
             <form action="/update_process" method="post">
@@ -89,7 +97,7 @@ app.get('/update/:pageId', function(req, res){
       );
       res.send(html);
     });
-  });
+  // });
 });
 
 app.post('/update_process', function (req, res) {
@@ -97,14 +105,12 @@ app.post('/update_process', function (req, res) {
   const id = reqBody.id;
   const title = reqBody.title;
   const description = reqBody.description;
-
   fs.rename(`data/${id}`, `data/${title}`, function (error) {
     fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
       res.redirect(`/page/${title}`);
     });
   });
 });
-
 app.post('/delete_process', function(req, res){
   const reqBody = req.body;
   const id = reqBody.id;
